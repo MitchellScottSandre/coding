481953f53b54588c0cef969de33e6eea8b6ec4e6
Scott Sandre <Scott Sandre>
2016-12-12 14:46:17 -0500

    starting to study for finals

--- cFiles/cs137/notes/examReview.c
+++ cFiles/cs137/notes/examReview.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
+//==============================Random Algorithms
+//GCD
+
+//PRIME
+
+//EUCLIDS
+
+//ERATOSTHENES
+
+//HORNERS
+
+//BISECTION
+
+//COUNTING CHANGE
+
+//===============================SORTING=======================
+//SELECTION SORT
+
+//INSERTION SORT
+
+//MERGE SORT
+
+//QUICK SORT
+
+//===============================Searching=======================
+//LINEAR SEARCH
+
+//BINARY SEARCH
+
+//=============================POINTERS FUN======================
+
 
 
 

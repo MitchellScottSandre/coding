465299c0d23409e1406515ce62d65e37b90f2d40
Scott Sandre <Scott Sandre>
2016-12-13 10:24:23 -0500

    blah

--- cFiles/cs137/notes/examReview.c
+++ cFiles/cs137/notes/examReview.c
@@ -163,15 +163,98 @@ void useThisToCallMergeSort(int a[], int n){
 }
 
 //QUICK SORT
+//quicksort needs: quicksort(int a[], int n), and it needs
+//partition (int a[], int n), and swap(int a, int b)
+/*Better Description from Online:
+1. partition array into left and right sub arrays such that
+	all elements in left sub array are less than elements in
+	right sub array
+2. recursively sort left and right sub arrays
+3. concatenate left and right sub arrays with pivot in middle
+
+How To Partition the Array:
+1 choose an elements fromm the array as a pivot
+2. move all elements < pivot to LEFT, all elements > pivot to RIGHT;
+*/
+void swap(int a[], int i, int j){
+	int temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
+}
+int partition(int a[], int n){//pivot is a[0]
+	int m = 0;
+	for (int i = 1; i < n; i++){
+		if (a[i] < a[0]){//comparing to pivot at a[0]
+			m++;//moving m up so it becomes middle
+			swap(a, m, i);
+		}
+	}
+	swap(a, 0, m);
+	return m;
+}
+void quickSort(int a[], int n){
+	if (n <= 1) return;
+	int m = partition(a, n);
+	quickSort(a, m);
+	quickSort(a + m + 1, n - m - 1);
+}
+
+//Standard Library qSort function..needs compareFunction
+
+int compareFunction(const void *a, const void *b){
+ 	const int *ptr1 = a;
+	const int *ptr2 = b;
+	int val1 = *ptr1;
+	int val2 = *ptr2;
+	if (val1 > val2)return 1;
+	if (val1 == val2)return 0;
+	return -1;
+}
+
+
 
 
 //===============================Searching=======================
 //LINEAR SEARCH
+//pretty darn easy. just look through entire array index by index and check values
+int linearSearch(int a[], int n, int val){
+	for (int i = 0; i < n; i++){
+		if (a[i] == val) return i;
+	}
+	return -1;//number could not be found
+}
 
 //BINARY SEARCH
+//basic idea: given a SORTED array, check middle elements a[m]
+//if a[m] is our value, return m
+//otherwise, if it is larger/smaller, search either the top or bottom half
 
+int binarySearch(int a[], int n, int val){
+	int lo = 0, hi = n - 1;
+	while (lo <= hi){
+		int m = lo + (hi - lo)/2; //we could do (lo + hi) / 2, but we dont want to add two numbers
+		//that are too big and overflow the stack! so do this instead
+		if (a[m] == val)return m;
+		if (a[m] > val) hi = m -1;//search bottom half
+		else lo = m + 1;//search top half
+	}
+	return -1;//didn't find it
+}
 //=============================POINTERS FUN======================
+//...no they are not.
+void swap_usingPointers(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
+int *indexLargestVal_usingPointers(int a[], int n){
+	int m = 0;//index of largest value thus far
+	for (int i = 1; i < n; i++){
+		if (a[i] > a[m]) m = i;
+	}
+	return a + m;//&(a[m]);//return location in a of largest val
+}
 
 //=============================STRUCTURES MORE FUN===============
 
@@ -231,12 +314,16 @@ int main(void){
 	int c[20] = {0};
 	int d[20] = {0};
 	int e[20] = {0};
+	int z[20] = {0};
+	int y[20] = {0};
 	for (int i = 0; i < len; i++){
 		//--> need to include <stdlib.h> to use rand()
 		b[i] = rand() % 100 - 50;//rand() produces a number between 0 and rand() max ( a big number)...so mod it
 		c[i] = rand() % 100 - 50;
 		d[i] = rand() % 100 - 50;
 		e[i] = rand() % 100 - 50;
+		z[i] = rand() % 100 - 50;
+		y[i] = rand() % 100 - 50;
 	}
 
 
@@ -252,6 +339,51 @@ int main(void){
 	useThisToCallMergeSort(d, len);
 	displayArray(d, len);
 
+	printf("Testing QUICK Sort Algorithm:\n");
+	quickSort(e, len);
+	displayArray(e, len);
+
+	printf("Testing standard library q sort:\n");
+	//need to make a compareFunction
+	//END OF SORTING
+	//qsort(base array, length of array, size of each index, compareFunction)
+	const int len_z = sizeof(z)/sizeof(int);
+	qsort(z, len_z, sizeof(z[0]), compareFunction);
+	displayArray(z, len_z);
+
+	printf("Testing LINEAR Search:\n");
+	int g[] = {1, 4, 6, 2, 3, 4, 2, -22, -55, -33, -44, 9, 98773, 222, 4444, -2222, -4, 8, 8, 2, 17, 88};
+	int len_g = sizeof(g)/sizeof(g[0]);
+	printf("The number 17 is located in g at index %d\n", linearSearch(g, len_g, 17));
+	printf("The number 133332 is located in g at index %d\n", linearSearch(g, len_g, 133332));
+
+	printf("Testing BINARY search - with a sorted array:\n");
+	int h[] = {1,2,3,4,5,6,7,8,9,10,22,44,55,66,88,123,145,177,188,231,234,256,278,300,312,3177,1000, 1000333};
+	const int len_h = sizeof(h)/sizeof(h[0]);
+	printf("The value 312 is located in h at index %d\n", binarySearch(h, len_h, 312));
+	printf("The value 13 is located in h at index %d\n", binarySearch(h, len_h, 13));
+
+	//=====================POINTERS====================
+	printf("Moving on to pointers...\n");
+	int i = 16;	//make int
+	printf("Before i is %d\n", i);
+	int *p;		//make pointer
+	p = &i;		//p points to the memory location of i
+	*p = 22;	//the value p points to is now 22. p points to memory address of i, therefore i is 22
+	printf("After i is %d\n", i);
+	printf("The value of p is also %d\n", *p);
+
+	int aa = 4, bb = 88;
+	printf("Before: a is %d, b is %d\n", aa, bb);
+	swap_usingPointers(&aa, &bb);
+	printf("Just swapped. After: a is %d, b is %d\n", aa, bb);
+
+	printf("Findinf largest value in array using pointers");
+	int *index_maxVal = indexLargestVal_usingPointers(y, sizeof(y)/sizeof(y[0]));
+	printf("Y is:\n");
+	displayArray(y, sizeof(y)/sizeof(y[0]));
+	printf("Largest val in y is %d\n", y[*index_maxVal]);//y at the value of largestValInY
+	printf("Why the hell isn't this working???\n");
 
 
 

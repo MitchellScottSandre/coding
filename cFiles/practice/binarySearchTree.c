e8e1f48933cdcac85045509960d4fd0c02fc4845
Scott Sandre <scott@Scotts-MacBook-Pro.local>
2016-12-25 14:13:23 -0500

    finished bst

--- cFiles/practice/binarySearchTree.c
+++ cFiles/practice/binarySearchTree.c
@@ -1,6 +1,134 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct BST {
+	int data;
+	struct BST *lchild, *rchild;
+} node;
+
+void insert(node * root, node *new_node){//this creates a binary search tree
+	if (new_node->data < root->data){//LESS THAN
+		if (root->lchild == NULL){
+			root->lchild = new_node; //it's null, so just set it to new node
+		} else {
+			insert(root->lchild, new_node);//try again on the left child node
+		}
+	}
+
+	if (new_node->data > root->data){//GREATER THAN
+		if (root->rchild == NULL){
+			root->rchild = new_node;
+		} else {
+			insert(root->rchild, new_node);
+		}
+	}
+}
+
+//displays the tree in INORDER fashion
+void inorder(node *temp){
+	if (temp != NULL){
+		inorder(temp->lchild);
+		printf("%d\n ", temp->data);
+		inorder(temp->rchild);
+	}
+}
+
+void preorder(node *temp){
+	if (temp != NULL){
+		printf("%d\n ", temp->data);
+		inorder(temp->lchild);
+		inorder(temp->rchild);
+	}
+}
+void postorder (node *temp){
+	inorder(temp->lchild);
+	inorder(temp->rchild);
+	printf("%d\n ", temp->data);
+}
+
+//searches for a specific node (with a value) in the Binary Search Tree
+node *search(node *root, int key, node **parent){//key is value we are looking for
+	node *temp;
+	temp = root;
+	while (temp != NULL){
+		if (temp->data == key){
+			printf("The %d Element is Present\n", temp->data);
+			return temp;
+		}
+		//else
+		*parent = temp;
+		if (temp->data > key){//so search left
+			temp = temp->lchild;
+		} else {
+			temp = temp->rchild;
+		}
+	}
+	return NULL;
+}
+
+node *get_node(){
+	node *temp;
+	temp = (node *) malloc(sizeof(node));
+	temp->lchild = NULL;
+	temp->rchild = NULL;
+	return temp;
+}
+
 
 int main(void){
+	int choice, key;
+	char ans = 'N';
+	node *new_node, *root, *tmp, *parent;
+	node *getNode();
+	root = NULL;
 
-  return 0;
+	printf("Program For Binary Search Tree\n");
+	do {
+		printf("\n1.Create");
+      	printf("\n2.Search");
+      	printf("\n3.Recursive Traversals");
+      	printf("\n4.Exit");
+      	printf("\nEnter your choice :");
+		scanf("%d", &choice);
+
+		switch(choice){
+			case 1:
+				do {
+					new_node = get_node();
+					printf("Enter the Data of New Node: ");
+					scanf("%d", &new_node->data);
+
+					if (root == NULL){
+						root = new_node;
+					} else {
+						insert(root, new_node);
+					}
+					printf("Enter more elements? y or n:");
+					scanf(" %c", &ans);//need space here!!!
+				} while (ans == 'y');
+				break;
+			case 2:
+				printf("Enter Element to be Searched: ");
+				scanf("%d", &key);
+				tmp = search(root, key, &parent);
+				//printf("Parent of node %d is %d\n", tmp->data, parent->data);
+				break;
+			case 3:
+				if (root == NULL){
+					printf("The tree is not created\n");
+				} else {
+						printf("The InOrder Display:\n");
+						inorder(root);
+						printf("The PREOrder Display:\n");
+						preorder(root);
+						printf("The POSTOrder Display:\n");
+						postorder(root);
+					}
+				break;
+		}
+
+	} while (choice != 4);
+
+
+	return 0;
 }
--- cFiles/practice/list.c
+++ cFiles/practice/list.c
@@ -49,8 +49,48 @@ int pop_front(node_t **head){
     free(*head);
     *head = nextNode;
     return retVal;
+}
 
+int remove_last(node_t *head){
+    int retVal = 0;
 
+    //if only one item in the list, remove it
+    if (head->next == NULL){
+        retVal = head->val;
+        free(head);
+        return retVal;
+    }
+
+    //else
+    node_t *current = head;
+    while (current->next->next != NULL){
+        current = current->next;
+    }
+
+    retVal = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retVal;
+}
+
+int remove_by_index(node_t **head, int n){
+    int retVal = -1;
+    node_t *current = *head;
+    node_t *temp_node = NULL;
+
+    if (n == 0){
+        return pop_front(head);
+    }
+
+    for (int i = 0; i < n-1; i++){
+        if (current->next == NULL) return -1;
+        current = current->next;
+    }
+    temp_node = current->next;
+    retVal = temp_node->val;
+    current->next = temp_node->next;
+    free(temp_node);
+    return retVal;
 }
 
 int main(void){
@@ -62,9 +102,16 @@ int main(void){
     head->next->next = NULL;
     push_back(head, 11);
     push_back(head, 1033);
+    push_back(head, 34);
+    push_back(head, 87);
+    push_back(head, -32);
     push_front(&head, 5);
     printList(head);
     printf("popping front: %d\n", pop_front(&head));
+    printList(head);
+    printf("removing last: %d\n", remove_last(head));
+    printList(head);
+    printf("removing index 2: %d\n",remove_by_index(&head, 2));
     printList(head);
 
 

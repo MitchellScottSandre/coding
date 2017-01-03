3b2bc12d2bfe525d9a5037ff8ef2b599d859c430
Scott Sandre <scott@Scotts-MacBook-Pro.local>
2017-01-03 14:19:52 -0500

    blah

---
+++ cFiles/cs138/week01/01_drinks.c
@@ -0,0 +1,11 @@
+using namespace std;
+#include<iostream>
+#include<string>
+
+const string kidDrink = "juice";//globals
+string adultDrink = "coffee";
+
+int main(int argc, char* argv[]){
+	cout << "hello world" << endl;
+	return 0;
+}

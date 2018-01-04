2b7686632673f63528955b82954b4b3813782282
MitchellScottSandre <msandre@edu.uwaterloo.ca>
2018-01-04 16:15:28 -0500

    started udemy react native course

--- portfolioAnalyzer/Analyzer/src/main/java/com/controllers/HomeController.java
+++ portfolioAnalyzer/Analyzer/src/main/java/com/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.controllers;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +21,18 @@ public class HomeController {
         System.out.println("TEST");
         System.out.println(stock.getQuote().getPrice());
 
+        String[] codes = {"TSLA", "F", "TM"};
+        String baseUrl = "http://finance.yahoo.com/q?s=";
+        String ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/27.0.1438.7 Safari/537.33";
+
+        for (String code : codes) {
+            String url = baseUrl + code;
+            Document doc  = Jsoup.connect(url).userAgent(ua).timeout(10*1000).get();
+            String price = doc.select(".time_rtq_ticker").first().text();
+            String name = doc.select(".title h2").first().text();
+
+            System.out.println(String.format("%s [%s] is trading at %s", name, code, price));
+        }
 
 
         ModelAndView modelAndView = new ModelAndView("home");

0df6a2f651a1ab8a5fa4207855b81bd022a8dc41
MitchellScottSandre <msandre@edu.uwaterloo.ca>
2018-01-05 09:44:57 -0500

    WIP: udemy course

--- udemy/reactNativeWorkspace/albums/index.js
+++ udemy/reactNativeWorkspace/albums/index.js
@@ -1,11 +1,11 @@
 // Import a libary to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
-
+import Header from './src/components/header';
 // Create a component
 const App = () => {
     return (
-        <Text>Some Text</Text>
+        <Header/>
     );
 };
 

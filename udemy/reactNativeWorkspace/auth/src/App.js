38d3c3de5117eb6ddb14de187bc7bc9d30e239b4
MitchellScottSandre <msandre@edu.uwaterloo.ca>
2018-01-07 19:32:02 -0500

    finished auth

--- udemy/reactNativeWorkspace/auth/src/App.js
+++ udemy/reactNativeWorkspace/auth/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import * as firebase from 'firebase';
-import { Header } from './components/common'; // don't need to specify the index file, by DEFAULT it will therefore go to index.js
+import { Header, Button, Spinner } from './components/common'; // don't need to specify the index file, by DEFAULT it will therefore go to index.js
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = {
+        loggedIn: null
+    }
+
     componentWillMount(){
         firebase.initializeApp({
             apiKey: "AIzaSyBl1V8XiIuwjxMq4Puyj2q1QNw6jktOzn0",
@@ -14,16 +18,53 @@ class App extends Component {
             storageBucket: "authentication-f2cc1.appspot.com",
             messagingSenderId: "654945123544"
           });
+
+        firebase.auth().onAuthStateChanged((user) => { //sign in, user... sign out, null
+            if (user) {
+                this.setState({loggedIn: true});
+            } else {
+                this.setState({loggedIn: false});
+            }
+        });
+    }
+
+    renderContent() {
+        switch(this.state.loggedIn){
+            case true:
+                return (
+                    <Button onPress={() => firebase.auth().signOut()} >
+                        Log Out
+                    </Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return (
+                    <View style={styles.mainSpinnerStyle}>
+                        <Spinner size="large" />
+                    </View>
+                );
+        }
+        
     }
 
     render(){
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    mainSpinnerStyle: {
+        marginTop: 20,
+        height: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+};  
 
 export default App;

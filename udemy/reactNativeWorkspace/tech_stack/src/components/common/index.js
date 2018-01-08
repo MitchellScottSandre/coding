36701958febd059616b2da1d42990ca076da8a47
MitchellScottSandre <msandre@edu.uwaterloo.ca>
2018-01-08 18:42:05 -0500

    wip tech stach

---
+++ udemy/reactNativeWorkspace/tech_stack/src/components/common/index.js
@@ -0,0 +1,7 @@
+// let's you put all of your reusable components in one file, let's you do just one import { aa, bb, cc } from /common instead of multiple imports
+export * from './Button'; // import and exports the button component
+export * from './Card';  // if you use this approach, can't use 'default' in the files
+export * from './CardSection';
+export * from './Header';
+export * from './InputField';
+export * from './Spinner';
